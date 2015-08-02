86a089653b0074b23f5176378e051a418cb54e04
dorian-marchal <marchal.dorian@gmail.com>
2015-08-02 21:49:51 +0200

    Ajoute le script de dev local

---
+++ local-dev/fix-ressources-url.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Ce fichier remplace la fonction GM_getResourceURL de Greasemonkey pour permettre au script
+ * de fonctionner avec les fichiers locaux sous Google Chrome.
+ */
+
+var getResource = GM_getResourceURL;
+
+GM_getResourceURL = function(resource) {
+  return 'data:;base64,' + getResource(resource);
+};
---
+++ local-dev/local-dev-config.default.sh
@@ -0,0 +1,14 @@
+# L'utilisation de ce script est documentée à cette adresse :
+# https://github.com/dorian-marchal/spawnkill/blob/master/documentation/LocalUserscriptDev.md
+
+# répertoire de développement du script
+dev_script_path="/path/to/www/jvc-better-news/"
+
+# nom du fichier principal du script
+main_script_file="jvc-better-news.user.js"
+
+# Spécifique à Firefox, inutile sous Chrome
+# Répertoire greasemonkey du script
+# /!\ Tout ce qui se trouve dans ce répertoire sera supprimé !
+# Veillez à ne pas vous tromper en saisissant cette variable !
+gm_script_path="/home/<user>/.mozilla/firefox/<profile_name>/gm_scripts/JVC_Better_News/"
