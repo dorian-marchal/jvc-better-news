d285627351cde1b6058556287cdbee524f777db7
dorian-marchal <marchal.dorian@gmail.com>
2015-08-16 22:11:35 +0200

    Ajoute le fonctionnement basique des boutons de news

--- js/news.js
+++ js/news.js
@@ -20,21 +20,31 @@
       url: null,
       el: null,
 
+      /**
+       * Ajoute les différents boutons de vote à la news
+       */
       addVoteButtons: function() {
         var imgWrapper = this.el.querySelector('.mask-img');
 
         var buttonsWrapper = document.createElement('div');
         buttonsWrapper.classList.add('news-vote-wrapper');
 
-        var createVoteButton = function(specificClass) {
+        var createVoteButton = function(specificClass, buttonText, clickListener) {
           var voteButton = document.createElement('button');
           voteButton.classList.add('news-vote', specificClass);
+          voteButton.textContent = buttonText;
+          voteButton.addEventListener('click', clickListener, false);
+
           buttonsWrapper.appendChild(voteButton);
           return voteButton;
         };
 
-        createVoteButton('vote-less');
-        createVoteButton('vote-more');
+        createVoteButton('vote-more', '+1', function() {
+          console.log('+1');
+        });
+        createVoteButton('vote-less', '-1', function() {
+          console.log('-1');
+        });
 
         imgWrapper.appendChild(buttonsWrapper);
       },
